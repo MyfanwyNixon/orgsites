e5a3b998e2d7b917c0d96672ba6d06c5154fed45
Matthew Somerville <matthew@mysociety.org>
2011-08-10 15:36:04 +0100

    Add company numbers, fixes #5, change header link.

--- mysocietyorg/web/wordpress_2_8_2/wp-content/themes/mysociety/footer.php
+++ mysocietyorg/web/wordpress_2_8_2/wp-content/themes/mysociety/footer.php
@@ -1,18 +1,21 @@
 
-		<div id="divFooter">
-			<?
-            if ($_SERVER['SERVER_NAME'] != 'cee.mysociety.org') {
-		    	print file_get_contents(TEMPLATEPATH . '/footer.html');
-            }
-			?>
+<div id="divFooter">
+<?
+    if ($_SERVER['SERVER_NAME'] != 'cee.mysociety.org') {
+        print file_get_contents(TEMPLATEPATH . '/footer.html');
+    }
+?>
 
-			<p>
-			mySociety is a project of <a href="http://www.ukcod.org.uk/">UK Citizens Online Democracy</a> (UKCOD). UKCOD is a registered charity in England and Wales, no. 1076346.
-			</p>
-		
-				<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
-		</div>
-	</div>
+<p>
+mySociety is a project of <a href="http://www.ukcod.org.uk/">UK Citizens Online Democracy</a> (UKCOD).
+UKCOD is a registered charity in England and Wales, no. 1076346.
+Its company number is 03277032, and mySociety Ltd's is 05798215.
+</p>
+
+<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
+
+</div>
+</div>
 </div>
 
 <?php

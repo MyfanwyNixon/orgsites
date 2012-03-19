36cd2c24f2f5ba82e0f41d8f7373aa17b7832f67
Edmund von der Burg <evdb@mysociety.org>
2012-03-19 20:24:32 +0000

    Proper vhosts.pl config entries

--- blogs/conf/vhosts-pl-extract.pl
+++ blogs/conf/vhosts-pl-extract.pl
@@ -1,5 +1,5 @@
 
-# For the actual bsites entry
+# For the actual sites entry
 
 $sites->{'blogs'} = {
     user           => 'mswww',
@@ -10,22 +10,41 @@ $sites->{'blogs'} = {
     crontab        => 0,
     exec_extras    => {
         user => [
+
             # get WP from their svn server
             'svn co http://core.svn.wordpress.org/tags/3.3.1 blogs/web;',
 
             # replace the default wp-config with our own
             'cp -pfuv blogs/conf/wp-config.php blogs/web/wp-config.php',
 
-            # uploads and plugins are both stored outside version control and are backed up
-            'rm -rf blogs/web/wp-content/uploads',
-            'rm -rf blogs/web/wp-content/plugins',
-            'mkdir -pv blog-uploads blog-plugins',
+            # uploads and plugins are both stored outside version control and
+            # are backed up
+            'rm -rfv blogs/web/wp-content/uploads',
+            'rm -rfv blogs/web/wp-content/plugins',
+            'mkdir -pv ../blog-uploads ../blog-plugins',
             'ln -sfv  ../../../../blog-uploads blogs/web/wp-content/uploads',
             'ln -sfv  ../../../../blog-plugins blogs/web/wp-content/plugins',
 
             # Themes are store under VC but not in the WP structure
-            'rm -rf blogs/web/wp-content/themes',
+            'rm -rfv blogs/web/wp-content/themes',
             'ln -sfv ../../themes blogs/web/wp-content/themes',
         ]
     },
+};
+
+# and then later
+$vhosts = {
+
+    # ...
+    'blogs.dev.mysociety.org' => {
+        site        => 'blogs',
+        staging     => 1,
+        servers     => ['eclipse'],
+        databases   => ['blogs-staging'],
+        user        => 'mswww-staging',
+        git_ref     => 'origin/blogs',
+        # backup_dirs => [ 'blog-uploads', 'blog-plugins' ],
+    },
+
+    # ...
 };

ef79fece9b97ec9d0cde6cbe099111c3e50c8efc
Edmund von der Burg <evdb@mysociety.org>
2012-03-20 15:04:19 +0000

    Correct BIN_DIR and tidy up a bit

--- blogs/bin/post_deploy.bash
+++ blogs/bin/post_deploy.bash
@@ -4,19 +4,21 @@
 set -e
 
 # make sure that we are in the correct directory
-BIN_DIR=$(      dirname $0            )
+BIN_DIR=$( cd `dirname $0` && pwd )
 BLOGS_DIR=$(    dirname $BIN_DIR      )
 ORGSITES_DIR=$( dirname $BLOGS_DIR    )
 VHOST_DIR=$(    dirname $ORGSITES_DIR )
 
+WEB_DIR=$BLOGS_DIR/web
+
 # get WP from their svn server
-svn co http://core.svn.wordpress.org/tags/3.3.1 $BLOGS_DIR/web;
+svn co http://core.svn.wordpress.org/tags/3.3.1 $WEB_DIR;
 
 ##### copy in the custom php files
 # change to this directory so that the paths are easier
-cd $BLOGS_DIR/custom_php
 echo "Copying php files from `pwd` to web dir"
 
+cd $BLOGS_DIR/custom_php
 for PHP_FILE in `find . -name '*.php'`; do
     DST_DIR=$( readlink -m ../web/`dirname $PHP_FILE` );
     mkdir -pv $DST_DIR;
@@ -28,13 +30,16 @@ done;
 mkdir -pv $VHOST_DIR/blog-uploads
 mkdir -pv $VHOST_DIR/blog-plugins
 mkdir -pv $VHOST_DIR/blogs.dir
-rm -rfv $BLOGS_DIR/web/wp-content/uploads
-rm -rfv $BLOGS_DIR/web/wp-content/plugins
-rm -rfv $BLOGS_DIR/web/wp-content/blogs.dir
-ln -sfv  $VHOST_DIR/blog-uploads $BLOGS_DIR/web/wp-content/uploads
-ln -sfv  $VHOST_DIR/blog-plugins $BLOGS_DIR/web/wp-content/plugins
-ln -sfv  $VHOST_DIR/blogs.dir    $BLOGS_DIR/web/wp-content/blogs.dir
+rm -rfv $WEB_DIR/wp-content/uploads
+rm -rfv $WEB_DIR/wp-content/plugins
+rm -rfv $WEB_DIR/wp-content/blogs.dir
+ln -sfv  $VHOST_DIR/blog-uploads $WEB_DIR/wp-content/uploads
+ln -sfv  $VHOST_DIR/blog-plugins $WEB_DIR/wp-content/plugins
+ln -sfv  $VHOST_DIR/blogs.dir    $WEB_DIR/wp-content/blogs.dir
 
 # Themes are store under VC but not in the WP structure
-rm -rfv $BLOGS_DIR/web/wp-content/themes
-ln -sfv $BLOGS_DIR/themes $BLOGS_DIR/web/wp-content/themes
+rm -rfv $WEB_DIR/wp-content/themes
+ln -sfv $BLOGS_DIR/themes $WEB_DIR/wp-content/themes
+
+echo "All done - $0 completed successfully"
+

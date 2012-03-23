066f2d16fbe0c84d467338e808f390e2bb810dc6
Josh Angell <josh@supercooldesign.co.uk>
2012-03-23 13:48:24 +0000

    Register product and project post types

--- blogs/themes/mysociety/functions.php
+++ blogs/themes/mysociety/functions.php
@@ -15,18 +15,44 @@
       ));
 
 
-  // add_action( 'init', 'create_post_type' );
-  // function create_post_type() {
-  //         register_post_type( 'uf_gig',
-  //                 array(
-  //                         'labels' => array(
-  //                                 'name' => __( 'Gigs' ),
-  //                                 'singular_name' => __( 'Gig' )
-  //                         ),
-  //                         'public' => true,
-  //                         'has_archive' => true,
-  //                         'rewrite' => array('slug' => 'gigs')
-  //                 )
-  //         );
-  // }
+  add_action( 'init', 'create_post_type' );
+  function create_post_type() {
+    
+    register_post_type(
+      'ms_project',
+      array(
+        'labels' => array(
+          'name'                => __( 'Projects' ),
+          'singular_name'       => __( 'Project' ),
+          'add_new_item'        => __( 'Add New Project'),
+          'search_items'        => __( 'Search Projects' ),
+          'not_found'           => __( 'No projects found' ),
+          'not_found_in_trash'  => __( 'No projects found in Trash' )
+        ),
+       'public'         => true,
+       'has_archive'    => true,
+       'menu_position'  => 5,
+       'rewrite'        => array('slug' => 'projects')
+      )
+    );
+
+    register_post_type(
+      'ms_product',
+      array(
+        'labels' => array(
+          'name'                => __( 'Products' ),
+          'singular_name'       => __( 'Product' ),
+          'add_new_item'        => __( 'Add New Product'),
+          'search_items'        => __( 'Search Products' ),
+          'not_found'           => __( 'No products found' ),
+          'not_found_in_trash'  => __( 'No products found in Trash' )
+        ),
+       'public'         => true,
+       'has_archive'    => true,
+       'menu_position'  => 5,
+       'rewrite'        => array('slug' => 'products')
+      )
+    );
+
+  }
 ?>

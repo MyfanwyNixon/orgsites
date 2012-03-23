9f7db0719151704110e84d15ac45ab2bfc79c95c
Josh Angell <josh@supercooldesign.co.uk>
2012-03-23 11:33:34 +0000

    Add placeholder polyfill

--- blogs/themes/mysociety/assets/js/mysociety.js
+++ blogs/themes/mysociety/assets/js/mysociety.js
@@ -2,6 +2,8 @@ $(function(){
   var $html = $('html');
 
   $html.removeClass('no-js').addClass('js');
+  
+  $('input, textarea').placeholder();
 
   //add mobile class based on .mq()
   //then if .mobile load in helper.js etc
--- blogs/themes/mysociety/footer.php
+++ blogs/themes/mysociety/footer.php
@@ -36,6 +36,7 @@
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<? bloginfo( 'template_url' )?>/assets/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
 
+  <script src="<? bloginfo( 'template_url' )?>/assets/js/libs/jquery.placeholder.min.js" charset="utf-8"></script>
   <script src="<? bloginfo( 'template_url' )?>/assets/js/mysociety.js"></script>
 
   <script>

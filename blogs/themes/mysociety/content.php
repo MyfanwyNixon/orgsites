960330bcbebeae20b771f54907ca5770d992534f
Josh Angell <josh@supercooldesign.co.uk>
2012-03-26 13:26:56 +0100

    Make a standard loop on blog listing page (home.php by default)

--- blogs/themes/mysociety/content.php
+++ blogs/themes/mysociety/content.php
@@ -1,9 +1,37 @@
-<article id="post-<? the_ID(); ?>" <? post_class(); ?>>
+<article id="post-<? the_ID() ?>" <? post_class() ?>>
 
   <header class="entry-header">
-    <h1 class="entry-title"><? the_title(); ?></h1>
+    <h1 class="entry-title"><a href="<? the_permalink() ?>"><? the_title() ?></a></h1>
+    <div class="entry-date"><? mysociety_posted_on() ?></div>
+    <? 
+      $categories_list = get_the_category_list( __( ', ' ) );
+      if ( $categories_list ):
+    ?>
+      <div class="entry-categories">
+        <? printf( __( '<span>Posted in:</span> %1$s' ), $categories_list ); ?>
+      </div>
+    <? endif; ?>
   </header>
 
   <div class="entry-content">
+    <? if(get_field('intro')): ?><p class="entry-intro"><? the_field('intro') ?></p><? endif; ?>
+    <? if(get_field('body')): ?><div class="entry-body"><? the_field('body') ?></div><? endif; ?>
 	</div>
+
+  <div class="entry-meta">
+    <?
+      $tags_list = get_the_tag_list( '', __( ', ' ) );
+      if ( $tags_list ):
+    ?>
+      <div class="entry-tags">
+        <? printf( __( '<span>Tagged:</span> %1$s' ), $tags_list ); ?>
+      </div>
+    <? endif; ?>
+
+    <?
+      if ( comments_open() && ! post_password_required() ) : 
+        comments_popup_link(  __( 'Comment' ), _x( '1 Comment', 'comments number' ), _x( '% Comments', 'comments number' ), 'comment-button' );
+      endif;
+    ?>
+  </div>
 </article>
--- blogs/themes/mysociety/functions.php
+++ blogs/themes/mysociety/functions.php
@@ -70,8 +70,8 @@
   // ADVANCED CUSTOM FIELDS
   // http://www.advancedcustomfields.com/
   /**
-  * Activate Add-ons
-  */ 
+   * Activate Add-ons
+   */ 
   if(!get_option('acf_repeater_ac')) update_option('acf_repeater_ac', "QJF7-L4IX-UCNP-RF2W");
 
   /**
@@ -83,5 +83,46 @@
     include('fields/projects-and-products.php');
   }
 
+
+  /*** CUSTOM FUNCTIONS ***/
+
+  /**
+   * Date Formatting
+   *
+   * Prints HTML with meta information for the current post-date/time and author.
+   * Modded from the WordPress Twenty Eleven theme
+   */
+  if ( ! function_exists( 'mysociety_posted_on' ) ) :
+  function mysociety_posted_on() {
+    printf( __( '
+      <time class="entry-date" datetime="%1$s" pubdate>%2$s</time>
+      <span class="author vcard">by <a class="url fn n" href="%3$s" title="%4$s" rel="author">%5$s</a></span>
+      ' ),
+      esc_attr( get_the_date( 'c' ) ),
+      esc_html( get_the_date( 'l, F jS, Y' ) ),
+      esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+      esc_attr( sprintf( __( 'View all posts by %s'), get_the_author() ) ),
+      get_the_author()
+    );
+  }
+  endif;
+
+  /**
+   * Content Navigation
+   *
+   * Display navigation to next/previous pages when applicable
+   */
+  if ( ! function_exists( 'mysociety_content_nav' ) ) {
+    function mysociety_content_nav( $nav_class ) {
+      global $wp_query;
+
+      if ( $wp_query->max_num_pages > 1 ) : ?>
+        <nav id="pagination" class="<?php echo $nav_class; ?>">
+          <div class="nav-previous"><?php next_posts_link( __( 'Previous' ) ); ?></div>
+          <div class="nav-next"><?php previous_posts_link( __( 'Next' ) ); ?></div>
+        </nav>
+      <?php endif;
+    }
+  }
 
 ?>

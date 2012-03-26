81979954a280cffb93b489da247fee3fef9371f8
Josh Angell <josh@supercooldesign.co.uk>
2012-03-26 11:33:29 +0100

    initial content loop

---
+++ blogs/themes/mysociety/content.php
@@ -0,0 +1,9 @@
+<article id="post-<? the_ID(); ?>" <? post_class(); ?>>
+
+  <header class="entry-header">
+    <h1 class="entry-title"><? the_title(); ?></h1>
+  </header>
+
+  <div class="entry-content">
+	</div>
+</article>
--- blogs/themes/mysociety/home.php
+++ blogs/themes/mysociety/home.php
@@ -1,5 +1,26 @@
 <? get_header(); ?>
   <div class="content" role="main">
-    home.php
+    <? if ( have_posts() ) : ?>
+
+      <? while ( have_posts() ) : the_post(); ?>
+
+        <? get_template_part( 'content' ); ?>
+
+      <? endwhile; ?>
+
+    <? else : ?>
+
+      <article id="post-0" class="post no-results not-found">
+        <header class="entry-header">
+          <h1 class="entry-title"><?php _e( 'Nothing Found' ); ?></h1>
+        </header>
+
+        <div class="entry-content">
+          <p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.' ); ?></p>
+          <?php get_search_form(); ?>
+        </div>
+      </article>
+
+    <?php endif; ?>
   </div>
 <? get_footer(); ?>

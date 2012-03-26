358473ae9c8614adaf24028fe9388a6326f44f88
Josh Angell <josh@supercooldesign.co.uk>
2012-03-26 17:22:05 +0100

    Echoing out product info

--- blogs/themes/mysociety/content-project_product.php
+++ blogs/themes/mysociety/content-project_product.php
@@ -7,12 +7,42 @@
     <p class="intro"><? the_field('intro') ?></p>
     <div class="body"><? the_field('body') ?></div>
     
-    <? if(get_field('gallery')): ?>
+    <?
+      $images = get_field('gallery');
+      if($images):
+    ?>
       <ul class="plain-list">
-      <? foreach(get_field('gallery') as $image): ?>
+      <? foreach($images as $image): ?>
         <li>
           <? $sized = vt_resize( $image[image], '', 250, 250, true ); ?>
           <img src="<?= $sized[url]; ?>" width="<?= $sized[width]; ?>" height="<?= $sized[height]; ?>" alt="<?= $image['alt'] ?>" />
+        </li>
+      <? endforeach; ?>
+      </ul>
+    <? endif; ?>
+
+    <?
+      if(get_field('build-your-own')) {
+        echo '<h2>Build your own</h2>';
+        echo the_field('build-your-own');
+      }
+    ?>
+
+    <?
+      if(get_field('commercial-options')) {
+        echo '<h2>Commercial Options</h2>';
+        echo the_field('commercial-options');
+      }
+    ?>
+
+    <?
+      $resources = get_field('resources');
+      if($resources):
+    ?>
+      <ul class="plain-list">
+      <? foreach($resources as $resource): ?>
+        <li>
+          <?= $resource[url]; ?> | <?= $resource[type]; ?>
         </li>
       <? endforeach; ?>
       </ul>

84d78145a747f7cf9f7ec7a4e16a3b6ba2d4a79d
Josh Angell <josh@supercooldesign.co.uk>
2012-03-26 10:40:26 +0100

    Add all the basic templates we will need as blank files

---
+++ blogs/themes/mysociety/front-page.php
@@ -0,0 +1,5 @@
+<? get_header(); ?>
+  <div class="content" role="main">
+    front-page
+  </div>
+<? get_footer(); ?>
---
+++ blogs/themes/mysociety/home.php
@@ -0,0 +1,5 @@
+<? get_header(); ?>
+  <div class="content" role="main">
+    home.php
+  </div>
+<? get_footer(); ?>

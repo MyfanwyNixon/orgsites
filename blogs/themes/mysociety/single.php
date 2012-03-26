7a44bb96c5796eafad770537354a2cd7cc1e71ed
Josh Angell <josh@supercooldesign.co.uk>
2012-03-26 13:42:31 +0100

    Make the single post view - needs comments

--- blogs/themes/mysociety/single.php
+++ blogs/themes/mysociety/single.php
@@ -1,1 +1,15 @@
-single.php
+<? get_header(); ?>
+  <div class="content" role="main">
+    <? while ( have_posts() ) : the_post();
+      get_template_part( 'content' );
+      comments_template( '', true );
+    ?>
+      <nav id="pagination" class="nav-single">
+            <span class="nav-previous"><? previous_post_link( '%link', __( 'Previous' ) ); ?></span>
+            <span class="nav-next"><? next_post_link( '%link', __( 'Next' ) ); ?></span>
+      </nav>
+    <?
+      endwhile;
+    ?>
+  </div>
+<? get_footer(); ?>

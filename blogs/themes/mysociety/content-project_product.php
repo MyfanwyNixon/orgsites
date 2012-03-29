d732b4beba0e03dba2320d272ee2cf53d385d98a
Josh Angell <josh@supercooldesign.co.uk>
2012-03-29 11:07:43 +0100

    Change width rules on desktop content and sidebar wrappers

--- blogs/themes/mysociety/content-project_product.php
+++ blogs/themes/mysociety/content-project_product.php
@@ -82,30 +82,35 @@
     <? endif; ?>
 
 
-    <? if($twitter_user): ?>
-      <section class="twitter_box">
-        <h2 class="twitter <?= $colour; ?>_icon">Twitter</h2>
-        <a class="<?= $colour; ?>_button" href="http://twitter.com/<?= $twitter_user; ?>">@<?= $twitter_user; ?></a>
-        <ul class="tweets">
-          <li>
-            <span>13 Mar</span>
-            <small><a href="http://ow.ly/9CvOe">http://ow.ly/9CvOe</a> "Ministers expect councils to make savings on road maintenance" - one way is to put problem-reporting online!</small>
-          </li>
-          <li>
-            <span>13 Mar</span>
-            <small>FixMyStreet is available for council websites: <a href="http://ow.ly/9CvUF">http://ow.ly/9CvUF</a> - slashes costs and makes reporting easy for users.</small>
-          </li>
-          <li>
-            <span class="tw_date">13 Mar</span>
-            <small>We're helping make the Philippines "a smoother, less perforated place": <a href="http://www.mysociety.org/2012/03/06/international-potholing/">http://www.mysociety.org/2012/03/06/international-potholing/</a></small>
-          </li>
-          <li>
-            <span class="tw_date">13 Mar</span>
-            <small>One of our most-viewed reports this week - strong smell coming from <a href="http://twitter.com/">#Hammersmith</a> park: <a href="http://ow.ly/9pblZ">http://ow.ly/9pblZ</a></small>
-          </li>
-        </ul>
-      </section>
-    <? endif; ?>
+      <div class="content-with-sidebar">
+        <h2 class="bubble <?= $colour; ?>_icon">News</h2>
+      </div>
+      <aside id="sidebar">
+        <? if($twitter_user): ?>
+          <section class="twitter_box">
+            <h2 class="twitter <?= $colour; ?>_icon">Twitter</h2>
+            <a class="<?= $colour; ?>_button" href="http://twitter.com/<?= $twitter_user; ?>">@<?= $twitter_user; ?></a>
+            <ul class="tweets">
+              <li>
+                <span>13 Mar</span>
+                <small><a href="http://ow.ly/9CvOe">http://ow.ly/9CvOe</a> "Ministers expect councils to make savings on road maintenance" - one way is to put problem-reporting online!</small>
+              </li>
+              <li>
+                <span>13 Mar</span>
+                <small>FixMyStreet is available for council websites: <a href="http://ow.ly/9CvUF">http://ow.ly/9CvUF</a> - slashes costs and makes reporting easy for users.</small>
+              </li>
+              <li>
+                <span class="tw_date">13 Mar</span>
+                <small>We're helping make the Philippines "a smoother, less perforated place": <a href="http://www.mysociety.org/2012/03/06/international-potholing/">http://www.mysociety.org/2012/03/06/international-potholing/</a></small>
+              </li>
+              <li>
+                <span class="tw_date">13 Mar</span>
+                <small>One of our most-viewed reports this week - strong smell coming from <a href="http://twitter.com/">#Hammersmith</a> park: <a href="http://ow.ly/9pblZ">http://ow.ly/9pblZ</a></small>
+              </li>
+            </ul>
+          </section>
+        <? endif; ?>
+      </aside>
     <!-- news -->
     <!-- pagination? -->
   </div>

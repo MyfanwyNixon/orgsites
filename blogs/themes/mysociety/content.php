81daca84dccba5dc0cbd1492be65ee230bfcd0a6
James Coleman <james@supercooldesign.co.uk>
2012-03-29 16:01:56 +0100

    More header bits
    Signed-off-by: James Coleman <james@supercooldesign.co.uk>

--- blogs/themes/mysociety/content.php
+++ blogs/themes/mysociety/content.php
@@ -1,7 +1,7 @@
 <article id="post-<? the_ID() ?>" <? post_class() ?>>
   <header class="entry-header">
     <h2 class="entry-date"><a href="<? the_permalink() ?>"><? the_title() ?></a></h2>
-    <p class="entry-date"><? mysociety_posted_on() ?></p>
+    <div class="entry-date"><? mysociety_posted_on() ?></div>
     <? 
       $categories_list = get_the_category_list( __( ', ' ) );
       if ( $categories_list ):

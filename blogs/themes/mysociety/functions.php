7de53845dd68afa787aa667083f58ff34244f3e2
Josh Angell <josh@supercooldesign.co.uk>
2012-04-04 10:52:10 +0100

    Fix single post page to be better designed and tweak the comments (for #6)

--- blogs/themes/mysociety/functions.php
+++ blogs/themes/mysociety/functions.php
@@ -178,8 +178,7 @@
                 /* translators: 1: comment author, 2: date and time */
                 printf( __( '%1$s %2$s', 'mysociety' ),
                   sprintf( '<span class="fn">%s</span>', get_comment_author_link() ),
-                  sprintf( '<a href="%1$s"><time pubdate datetime="%2$s">%3$s</time></a>',
-                    esc_url( get_comment_link( $comment->comment_ID ) ),
+                  sprintf( '<time pubdate datetime="%1$s">%2$s</time>',
                     get_comment_time( 'c' ),
                     /* translators: 1: date, 2: time */
                     sprintf( __( '%1$s at %2$s', 'mysociety' ), get_comment_date(), get_comment_time() )
@@ -196,10 +195,11 @@
             <? endif; ?>
           </header>
 
-          <div class="comment-content"><?php comment_text(); ?></div>
+          <div class="comment-content"><? comment_text(); ?></div>
 
           <div class="reply">
-            <?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply', 'mysociety' ), 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+            <? comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply', 'mysociety' ), 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+            <a class="comment-permalink" href="<?= esc_url( get_comment_link( $comment->comment_ID ) ) ?>"><? _e( 'Permalink', 'mysociety') ?></a>
           </div><!-- .reply -->
         </article><!-- #comment-## -->
       <?

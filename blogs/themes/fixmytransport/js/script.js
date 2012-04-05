3f6cd739b394307c4a72a0664162b42c58d2c032
Jedidiah Broadbent <hello@jedidiah.eu>
2012-04-05 13:18:14 +0100

    FMT Blog: Added animation on hover/tap

--- blogs/themes/fixmytransport/js/script.js
+++ blogs/themes/fixmytransport/js/script.js
@@ -3,6 +3,18 @@
 */
 
 
+jQuery(document).ready(function() {
+	if(Modernizr.cssanimations && Modernizr.csstransforms3d){
+		$('#banner')
+			.addClass('animated')
+			.html('<div class="anim-bg countryside"></div><div class="anim-bg2 train"></div><div class="anim-frame train"></div><div class="anim-people people1"></div>');
+		if(Modernizr.touch){
+			$('#banner').live('tap', function(event) {
+				$(this).toggleClass('tapplay');
+			});
+		}
+	}
+});
 
 
 

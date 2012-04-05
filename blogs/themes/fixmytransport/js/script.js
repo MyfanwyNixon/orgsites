7827e16e8c81a4ed1dfd968204cd700c7760c5b5
Jedidiah Broadbent <hello@jedidiah.eu>
2012-04-05 13:24:46 +0100

    FMT Blog: Replaced 'live' with 'on' and 'tap' with 'touchstart'

--- blogs/themes/fixmytransport/js/script.js
+++ blogs/themes/fixmytransport/js/script.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function() {
 			.addClass('animated')
 			.html('<div class="anim-bg countryside"></div><div class="anim-bg2 train"></div><div class="anim-frame train"></div><div class="anim-people people1"></div>');
 		if(Modernizr.touch){
-			$('#banner').live('tap', function(event) {
+			$('#banner').on('touchstart', function(event) {
 				$(this).toggleClass('tapplay');
 			});
 		}

6110c79a1b92b57ea33dc781a6a4eeab3f34f410
BenJam <benjam@nonsenselondon.com>
2012-05-17 08:52:37 +0100

    basic team page template
    as above

--- blogs/themes/mysociety/team.php
+++ blogs/themes/mysociety/team.php
@@ -1,6 +1,22 @@
+<h1>Team.php</h1>
 <?php
 /*
 Template Name: Team
 */
+?>
+<?php
+	$values = get_field('people');
+	if($values){
+		echo '<ul>';
+			foreach($values as $value){
+				foreach($value as $v){
+					echo '<li>' . $v . '</li>';
+				}
+			}
+		echo '</ul>';
+	}
+
+	// always good to see exactly what you are working with
+	var_dump($values);
 ?>
 <? get_footer(); ?>

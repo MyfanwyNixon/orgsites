8e530aa806e838a4bf52e404a354c08de4bddf26
BenJam <benjam@nonsenselondon.com>
2012-05-17 14:51:08 +0100

    Basic Templates
    Basic templates for each of the separate 'project' sections.

--- blogs/themes/mysociety/single-ms_public.php
+++ blogs/themes/mysociety/single-ms_public.php
@@ -1,2 +1,28 @@
 <!-- Public Page -->
-<h1>MS_Public.php</h1>
+<?php get_header(); ?>
+
+<h1><?php the_title();?></h1>
+
+<h2><?php echo get_field('strapline')?> </h2>
+
+<?php if(get_field('image')) :?>
+	<div>
+		<img src='<?php echo get_field('image')?>' alt='<?php the_title().' image';?>'>
+	</div>
+<?php endif ?>
+
+<p><?php echo get_field('overview')?> </p>
+
+<?php if(get_field('resources')) : ?>
+	<h3>Resources</h3>
+	<ul>
+	<?php foreach(get_field('resources') as $resource): ?>
+	    <li>
+			<a href='<?php echo $resource['url']; ?>'><?php echo $resource['text']; ?></>
+		</li>
+	<?php endforeach; ?>
+	</ul>
+<?php endif ?>
+
+
+<?php get_footer();?>

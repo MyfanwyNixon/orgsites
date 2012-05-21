a68c57301f98511a70f7d3cae745238133b28bc6
Jedidiah Broadbent <hello@jedidiah.eu>
2012-05-21 12:46:02 +0100

    mysociety.org: Added fancy zoom to enlarge the screenshot thumbnails

--- blogs/themes/mysociety/footer.php
+++ blogs/themes/mysociety/footer.php
@@ -20,3 +20,6 @@
 <script src="<?php bloginfo('template_url'); ?>/js/libs/jquery-1.7.2.min.js" type="text/javascript" charset="utf-8"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/libs/jquery.hoverintent.min.js" type="text/javascript" charset="utf-8"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/libs/jquery.isotope.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/FancyZoom.js" type="text/javascript" charset="utf-8"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/FancyZoomHTML.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript">setupZoom();</script>

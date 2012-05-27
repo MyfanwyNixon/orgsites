2da5858840d124780c21e47c3976e1c21e86927a
BenJam <benjam@nonsenselondon.com>
2012-05-27 13:02:59 +0100

    International tempalte f:1h
    adding international template, currently fields included are ONLY
    
    github > code
    wiki > project wiki
    document > project install guide (probably the wiki)
    
    @jedidiah we need to do some other standard sections for this. I'll create a ticket

--- blogs/themes/mysociety/single-ms_international.php
+++ blogs/themes/mysociety/single-ms_international.php
@@ -28,20 +28,43 @@
 				<p><?php echo get_field('overview')?></p>
 				
 			</header>
-			
-			<?php if(count(get_field('features')) > 0):?>
+	
+			<?php
+				//prepare some specific resources 
+				$resources = get_field('resources');
+				if(count($resources) > 0 ){
+					//split these up into the relative bits
+					foreach($resources as $resource){
+						switch ($resource['type']) {
+						    case 'github':
+						        $github = $resource;
+						        break;
+						    case 'wiki':
+						        $wiki = $resource;
+						        break;
+							case 'document':
+						        $document = $resource;
+						        break;
+						}
+					}
+				}
+			?>
 			<section class="product-facets">
-				<h3>Features <em><?php echo get_field('feature_text');?></em></h3>
-				<ul>
-					<?php foreach(get_field('features') as $feature):?><li>
-							<h3><?php echo $feature['heading'];?></h3>
-							<p><?php echo $feature['detail'];?></p>
-							<?php // TODO ADD BUTTON LINK! ?>
-							<p><a class="btn fms" href="#">A default link <strong>TODO</strong></a></p>
-					</li><?php endforeach ?>
-				</ul>
-			</section>
-			<?php endif?>			
+				<?php //TODO add these as ACF repeaters instead ?>
+				<ul><?php if(isset($document)):?><li>
+						<h3>What you will need</h3>
+						<p>We have written some non-technical guides detailing the things you will need to setup your own version.</p>
+						<p><a class="btn fms" href="<?php echo $document['url'];?>"><img src="<?php echo get_bloginfo('template_directory');?>/img/icons/general/16/info-white.png" style="vertical-align: text-top;opacity:0.5" alt=""> <?php echo $document['text'];?></a></p>
+					</li><?php endif ?><?php if(isset($wiki)):?><li>
+						<h3>Read the documentation</h3>
+						<p>Everything a developer should need to get going is in the wiki, if anything is missing let us know or write it yourself.</p>
+						<p><a class="btn fms" href="<?php echo $wiki['url'];?>"><img src="<?php echo get_bloginfo('template_directory');?>/img/icons/general/16/docs-white.png" style="vertical-align: middle;opacity:0.5" alt=""> <?php echo $wiki['text'];?></a></p>
+					</li><?php endif ?><?php if(isset($github)):?><li>
+						<h3>Download the code</h3>
+						<p>Everything is packaged up on Github ready for downloading, forking and contributing your changes back to.</p>
+						<p><a class="btn fms" href="<?php echo $github['url'];?>"><img src="<?php echo get_bloginfo('template_directory');?>/img/icons/general/16/download-white.png" style="vertical-align: text-top;opacity:0.5" alt=""> <?php echo $github['text'];?></a></p>
+					</li><?php endif ?></ul>	
+			</section>		
 			
 		</article>		
 		

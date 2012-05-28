ffe1a01398c3bba63bd974f5fab03e537e092bbc
BenJam <benjam@nonsenselondon.com>
2012-05-28 10:50:40 +0100

    Adding loop to custom page templates

--- blogs/themes/mysociety/donate.php
+++ blogs/themes/mysociety/donate.php
@@ -3,4 +3,16 @@
 Template Name: Donate
 */
 ?>
-<? get_footer(); ?>
+<?php get_header(); ?>
+	<body>
+	<h1><?php the_title(); ?></h1>
+	
+	<?php
+		if (have_posts()) : while (have_posts()) : the_post();
+
+		the_content();
+
+		endwhile; endif;
+	 ?>
+	</body>
+<?php get_footer(); ?>
--- blogs/themes/mysociety/hireus.php
+++ blogs/themes/mysociety/hireus.php
@@ -5,8 +5,15 @@ Template Name: Hire Us
 ?>
 
 <?php get_header(); ?>
-	
+	<body>
 	<h1><?php the_title(); ?></h1>
-	<?php the_content(); ?>
 	
+	<?php
+		if (have_posts()) : while (have_posts()) : the_post();
+
+		the_content();
+
+		endwhile; endif;
+	 ?>
+	</body>
 <?php get_footer(); ?>

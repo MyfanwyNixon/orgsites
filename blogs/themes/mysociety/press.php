cac4ad5643e30645de3f98b81095ace1f8c0b011
BenJam <benjam@nonsenselondon.com>
2012-05-28 19:23:04 +0100

    Adding press page template

---
+++ blogs/themes/mysociety/press.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Press
+*/
+?>
+<?php get_header(); ?>
+	<body>
+	<h1><?php the_title(); ?></h1>
+	
+	<?php
+		if (have_posts()) : while (have_posts()) : the_post();
+
+		the_content();
+
+		endwhile; endif;
+	 ?>
+	</body>
+<?php get_footer(); ?>

af0684e64be58a73be06001cfdcab8674993bf9c
BenJam <benjam@nonsenselondon.com>
2012-06-01 15:28:25 +0100

    adding a public site resource to boierplate on international pages

--- blogs/themes/mysociety/single-ms_international.php
+++ blogs/themes/mysociety/single-ms_international.php
@@ -38,6 +38,9 @@
 					//split these up into the relative bits
 					foreach($resources as $resource){
 						switch ($resource['type']) {
+							case 'public':
+						        $public = $resource;
+						        break;
 						    case 'github':
 						        $github = $resource;
 						        break;
@@ -53,15 +56,23 @@
 			?>
 			<section class="product-facets">
 				<?php //TODO add these as ACF repeaters instead ?>
-				<ul><?php if(isset($document)):?><li>
+				<ul>
+					<?php if(isset($document)):?><li>
 						<h3>What you will need</h3>
 						<p>We have written some non-technical guides detailing the things you will need to setup your own version.</p>
 						<p><a class="btn fms" href="<?php echo $document['url'];?>"><img src="<?php echo get_bloginfo('template_directory');?>/img/icons/general/16/info-white.png" style="vertical-align: text-top;opacity:0.5" alt=""> <?php echo $document['text'];?></a></p>
-					</li><?php endif ?><?php if(isset($wiki)):?><li>
+					</li>
+					<?php endif ?><?php if(isset($public)):?><li>
+						<h3>Check Out Our Site</h3>
+						<p>We run a version ourselves, have a snoop around to see what you can achieve with our tools.</p>
+						<p><a class="btn fms" href="<?php echo $wiki['url'];?>"><img src="<?php echo get_bloginfo('template_directory');?>/img/icons/general/16/info-white.png" style="vertical-align: middle;opacity:0.5" alt="">
+					<?php endif ?>
+					<?php if(isset($wiki)):?><li>
 						<h3>Read the documentation</h3>
 						<p>Everything a developer should need to get going is in the wiki, if anything is missing let us know or write it yourself.</p>
 						<p><a class="btn fms" href="<?php echo $wiki['url'];?>"><img src="<?php echo get_bloginfo('template_directory');?>/img/icons/general/16/docs-white.png" style="vertical-align: middle;opacity:0.5" alt=""> <?php echo $wiki['text'];?></a></p>
-					</li><?php endif ?><?php if(isset($github)):?><li>
+					</li><?php endif ?>
+					<?php if(isset($github)):?><li>
 						<h3>Download the code</h3>
 						<p>Everything is packaged up on Github ready for downloading, forking and contributing your changes back to.</p>
 						<p><a class="btn fms" href="<?php echo $github['url'];?>"><img src="<?php echo get_bloginfo('template_directory');?>/img/icons/general/16/download-white.png" style="vertical-align: text-top;opacity:0.5" alt=""> <?php echo $github['text'];?></a></p>

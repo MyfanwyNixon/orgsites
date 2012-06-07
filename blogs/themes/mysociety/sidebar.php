6c2bdf23aebd99c1f53c75f9a5cdd5d208ef1b69
BenJam <benjam@nonsenselondon.com>
2012-06-07 13:42:03 +0100

    Basic sidebar
    Adding basic sidebar as per Jed's design

---
+++ blogs/themes/mysociety/sidebar.php
@@ -0,0 +1,37 @@
+<div class="blogs-meta">
+
+	<section class="blogkey">
+		<h3>Our Blogs:</h3>
+		<ul>
+			<li><a class="mysociety" href="<?php get_bloginfo( 'url', 'raw' );?>">Company Blog</a></li>
+			<li><a class="fms" href="#">FixMyStreet Blog</a></li>
+			<li><a class="fmt" href="#">FixMyTransport Blog</a></li>
+			<li><a class="twfy" href="#">TheyWorkForYou Blog</a></li>
+			<li><a class="" href="#">Bloggity Blog Blog</a></li>
+		</ul>
+	</section>
+	
+	<?php
+		//TODO
+		// <section class="widget widget_search">
+		// 	<h3 class="widget_title">Search posts:</h3>
+		//  php get_search_form();
+		// 	</form>
+		// </section>
+	?>
+	
+	
+	<?php $args = array(
+	    'orderby'            => 'count',
+		'number'			 => 10,
+		'title_li'			 => '',
+		'depth'				 => -1
+		);
+	?>
+	<section class="widget widget_categories">
+		<h3 class="widget_title"><i class="icon-black-tag"></i>Categories</h3>
+		<ul>
+			<?php wp_list_categories( $args ); ?> 
+		</ul>
+	</section>
+</div>

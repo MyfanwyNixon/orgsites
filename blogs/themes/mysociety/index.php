bf5e9fd869262f92259f785165b734c7db338978
BenJam <benjam@nonsenselondon.com>
2012-06-07 14:26:47 +0100

    moving index loop into loop.php
    as FMT does.

--- blogs/themes/mysociety/index.php
+++ blogs/themes/mysociety/index.php
@@ -1,26 +1,14 @@
 <?php get_header();?>
- <!-- Start the Loop. -->
+
+<?php if (is_search()) : ?>		
+	<h1 class="pagetitle">Search Results: &ldquo;<?php the_search_query(); ?>&rdquo; <?php if (get_query_var('paged')) echo ' &mdash; Page '.get_query_var('paged'); ?></h1>
+<?php endif ?>
+
 <?php if ( have_posts() ) :?> 
 	<div class="blog-entries">
-		<?php while ( have_posts() ) : the_post(); ?>
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-
-			<header>
-				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-				<p class="meta">Written by <?php the_author_posts_link() ?> on <time datetime="<?php the_time('Y-m-d')?>">Posted <strong><?php the_time('jS F Y') ?></strong></time> in <?php the_category(', '); ?></p>
-			</header>
-		
-			<p class="entry">
-			  <?php the_content(); ?>
-			</p>
-
-			<footer>
-				<?php if ( comments_open() ) : ?><a class="comment" href="<?php the_permalink(); ?>#comments"><?php comments_number('0 Comments', '1 Comment', '% Comments'); ?></a><?php endif; ?>
-				<?php the_tags('<span class="tags">Tagged as ', ', ', '</span>'); ?>
-			</footer>
-		</article>	
+		<?php get_template_part('loop')?>
 	</div>
-<?php endwhile; else: ?>
+<?php else: ?>
 	<p>Sorry, no posts matched your criteria.</p>
 <?php endif; ?>
 
---
+++ blogs/themes/mysociety/loop.php
@@ -0,0 +1,18 @@
+<?php while ( have_posts() ) : the_post(); ?>
+<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+
+	<header>
+		<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+		<p class="meta">Written by <?php the_author_posts_link() ?> on <time datetime="<?php the_time('Y-m-d')?>">Posted <strong><?php the_time('jS F Y') ?></strong></time> in <?php the_category(', '); ?></p>
+	</header>
+
+	<p class="entry">
+	  <?php the_content(); ?>
+	</p>
+
+	<footer>
+		<?php if ( comments_open() ) : ?><a class="comment" href="<?php the_permalink(); ?>#comments"><?php comments_number('0 Comments', '1 Comment', '% Comments'); ?></a><?php endif; ?>
+		<?php the_tags('<span class="tags">Tagged as ', ', ', '</span>'); ?>
+	</footer>
+</article>	
+<?php endwhile;?>

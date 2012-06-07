ca2e022ed5d3e961cfb41ee3baa38dbfbab6d2c1
BenJam <benjam@nonsenselondon.com>
2012-06-07 16:11:26 +0100

    Header and markup
    Adding headers to blog pages and adding wrapper spans on anchors to CSS can creep in imagery (wrong way to do it)

--- blogs/themes/mysociety/single.php
+++ blogs/themes/mysociety/single.php
@@ -1,29 +1,34 @@
 <?php get_header();?>
- <!-- Start the Loop. -->
-<?php if ( have_posts() ) : ?>
+<header class="header">
+	<h1 class="logo"><a href="<?php echo get_bloginfo( 'url', 'raw' ); ?>">mySociety</a></h1>
+</header>
+<div id='content' class='blog'>
+	 <!-- Start the Loop. -->
+	<?php if ( have_posts() ) : ?>
 	
-	<div class="blog-entries">
-		<?php while ( have_posts() ) : the_post(); ?>
+		<div class="blog-entries">
+			<?php while ( have_posts() ) : the_post(); ?>
 
-		<article class="blog-post" id="post-<?php the_ID(); ?>">
+			<article class="blog-post" id="post-<?php the_ID(); ?>">
 
-			<header>
-				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-				<p class="meta">Written by <?php the_author_posts_link(); ?> on <?php the_time('F jS, Y') ?> in <?php the_category(', '); ?></p>
-			</header>
+				<header>
+					<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+					<p class="meta">Written by <span class="author-link"><?php the_author_posts_link(); ?></span> on <?php the_time('F jS, Y') ?> in <span class='catagory-link'><?php the_category(', '); ?></span></p>
+				</header>
 		
-		    <?php the_content(); ?>
+			    <?php the_content(); ?>
 
-			<footer>
-				<?php the_tags('<span class="tags">Tagged as ', ', ', '</span>'); ?>
-			</footer>
+				<footer>
+					<?php the_tags('<span class="tags">Tagged as ', ', ', '</span>'); ?>
+					<?php comments_template(); ?>
+				</footer>
 			
-			<?php comments_template(); ?>
-		</article>	
-	
-	</div>
-<?php endwhile; else: ?>
-	<p>Sorry, no posts matched your criteria.</p>
-<?php endif; ?>
 
-<?php get_sidebar(); ?>
+			</article>	
+	
+		</div>
+	<?php endwhile; else: ?>
+		<p>Sorry, no posts matched your criteria.</p>
+	<?php endif; ?>
+	<?php get_sidebar(); ?>
+</div>

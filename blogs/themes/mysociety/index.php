37b8ce77b9f6b931f209df923babaaa82129502c
BenJam <benjam@nonsenselondon.com>
2012-06-07 13:42:23 +0100

    Basic blog page and single page
    as per Jed's design.

--- blogs/themes/mysociety/index.php
+++ blogs/themes/mysociety/index.php
@@ -1,12 +1,27 @@
-<h1>Index.php</h1>
-<?php
-get_header();
-if (have_posts()) :
-   while (have_posts()) :
-      the_post();
-      the_content();
-   endwhile;
-endif;
-get_sidebar();
-get_footer(); 
-?>
+<?php get_header();?>
+ <!-- Start the Loop. -->
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+	<article class="blog-post">
+
+		<header>
+			<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+			<p class="meta">Written by <?php the_author_posts_link() ?> on <?php the_time('F jS, Y') ?> in <?php the_category(', '); ?></p>
+		</header>
+		
+		<p class="entry">
+		  <?php the_content(); ?>
+		</p>
+
+		<footer>
+			<?php comments_link(); ?> 
+		</footer>
+		
+	</article>	
+<?php endwhile; else: ?>
+	<p>Sorry, no posts matched your criteria.</p>
+<?php endif; ?>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer();?>
--- blogs/themes/mysociety/single.php
+++ blogs/themes/mysociety/single.php
@@ -1,3 +1,25 @@
-<!-- a single blog post view -->
-<h1>Single.php</h1>
-<? get_footer(); ?>
+<?php get_header();?>
+ <!-- Start the Loop. -->
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+	<article class="blog-post">
+
+		<header>
+			<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+			<p class="meta">Written by <?php the_author_posts_link() ?> on <?php the_time('F jS, Y') ?> in <?php the_category(', '); ?></p>
+		</header>
+		
+		<p class="entry">
+		  <?php the_content(); ?>
+		</p>
+
+		<footer>
+			<?php get_comments(); ?> 
+		</footer>
+		
+	</article>	
+<?php endwhile; else: ?>
+	<p>Sorry, no posts matched your criteria.</p>
+<?php endif; ?>
+
+<?php get_sidebar(); ?>

7a924fedef7074362501e27e254e266bdc8bdfbb
BenJam <benjam@nonsenselondon.com>
2012-06-07 19:51:32 +0100

    breaking resources out into a reusable piece of temaplte

--- blogs/themes/mysociety/404.php
+++ blogs/themes/mysociety/404.php
@@ -1,11 +1,8 @@
 <?php get_header(); ?>
 <body id="mysociety-org" class="productpage for_orgs fms">
-	
-	<header class="header">
-		<h1 class="logo"><a href="<?php echo get_bloginfo( 'url', 'raw' ); ?>">mySociety</a></h1>
-	</header>
+	<?php get_template_part('logo'); ?>
 	<h1>404</h1>
 	<p>What the duece?</p>
-<?php get_footer(); ?>
+	<?php get_footer(); ?>
 </body>
 </html>

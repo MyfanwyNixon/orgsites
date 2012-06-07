d20a0f549819183b2208390f77e7f5320ae22777
Jedidiah Broadbent <hello@jedidiah.eu>
2012-06-07 21:54:02 +0100

    mysociety.org: corrected minified variable

--- blogs/themes/mysociety/js/scripts.js
+++ blogs/themes/mysociety/js/scripts.js
@@ -1,7 +1,7 @@
 // Equal heights plugin
 (function(a){a.fn.equalHeights=function(d,c){var b=0;b=(d)?d:0;this.each(function(){if(a(this).height()>b){b=a(this).height();}});if((c)&&b>c){b=c;}return this.each(function(){a(this).height(b).css("overflow","auto");});};})(jQuery);
 // Temp col count fix
-tempcolfix=function(){var a=$(".section-options ul").removeClass("four_up").removeClass("three_up").removeClass("two_up").children("li").length;var b="four_up";if(!section-options-count%3){b="three_up";}else{if(!section-options-count%4){b="four_up";}else{if(!section-options-count%2){b="two_up";}}}$(".section-options ul").addClass(b);};
+tempcolfix=function(){var a=$(".section-options ul").removeClass("four_up").removeClass("three_up").removeClass("two_up").children("li").length;var b="four_up";if(!a%3){b="three_up";}else{if(!a%4){b="four_up";}else{if(!a%2){b="two_up";}}}$(".section-options ul").addClass(b);};
 
 $(document).ready(function() {
 	

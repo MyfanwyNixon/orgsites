93d29bcfb306170e8af057e6c556ce65f92f8ee1
Jedidiah Broadbent <hello@jedidiah.eu>
2012-06-07 16:47:03 +0100

    mysociety.org: Added basic styles to about page

--- blogs/themes/mysociety/about.php
+++ blogs/themes/mysociety/about.php
@@ -13,14 +13,36 @@ Template Name: About
 		<h1 class="logo"><a href="<?php echo get_bloginfo( 'url', 'raw' ); ?>">mySociety</a></h1>
 	</header>
 	
-	<h1><?php the_title(); ?></h1>
-	
-	<?php
+	<div id="content">
+		
+		<article class="products">
+			
+			<article class="article">
+			
+				<header>
+					<h2><?php the_title(); ?></h2>
+					<!-- <p class="meta">Written by <a href="#">Author Name</a> on <time datetime="2012-06-01">1st June 2012</time></p> -->
+				</header>
+				
+				<?php
 		if (have_posts()) : while (have_posts()) : the_post();
 
 		the_content();
 
 		endwhile; endif;
 	 ?>
+				
+			</article>
+			
+			
+			
+		</article>
+		
+		
+		
+		
+	</div>
+	
+	
 	</body>
 <?php get_footer(); ?>

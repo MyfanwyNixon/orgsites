393e729ab32e2a75cc2aec2061626b85d42cc10e
Jedidiah Broadbent <hello@jedidiah.eu>
2012-06-07 22:13:50 +0100

    fixed modulo in javascript

--- blogs/themes/mysociety/js/scripts.js
+++ blogs/themes/mysociety/js/scripts.js
@@ -1,7 +1,7 @@
 // Equal heights plugin
 (function(a){a.fn.equalHeights=function(d,c){var b=0;b=(d)?d:0;this.each(function(){if(a(this).height()>b){b=a(this).height();}});if((c)&&b>c){b=c;}return this.each(function(){a(this).height(b).css("overflow","auto");});};})(jQuery);
 // Temp col count fix
-var tempcolfix=function(){var a=$(".section-options ul").removeClass("four_up").removeClass("three_up").removeClass("two_up").children("li").length;var b="three_up";if(!a%3){b="three_up";}else{if(!a%4){b="four_up";}else{if(!a%2){b="two_up";}}}$(".section-options ul").addClass(b);};
+var tempcolfix=function(){var a=$(".section-options ul").removeClass("four_up").removeClass("three_up").removeClass("two_up").children("li").length;var b="three_up";if(a%3===0){b="three_up";}else{if(a%4===0){b="four_up";}else{if(a%2===0){b="two_up";}}}$(".section-options ul").addClass(b);};
 
 $(document).ready(function() {
 	

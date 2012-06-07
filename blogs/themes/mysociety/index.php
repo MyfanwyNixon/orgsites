f4e2385952a3d0a870c4acda90c829700d3c118a
BenJam <benjam@nonsenselondon.com>
2012-06-07 13:57:31 +0100

    Adding comments
    adding comments from the FMS blog template as they *should* match jed's design.

--- blogs/themes/mysociety/index.php
+++ blogs/themes/mysociety/index.php
@@ -1,23 +1,25 @@
 <?php get_header();?>
  <!-- Start the Loop. -->
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php if ( have_posts() ) :?> 
+	<div class="blog-entries">
+		<?php while ( have_posts() ) : the_post(); ?>
+		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 
-	<article class="blog-post">
-
-		<header>
-			<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-			<p class="meta">Written by <?php the_author_posts_link() ?> on <?php the_time('F jS, Y') ?> in <?php the_category(', '); ?></p>
-		</header>
+			<header>
+				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+				<p class="meta">Written by <?php the_author_posts_link() ?> on <time datetime="<?php the_time('Y-m-d')?>">Posted <strong><?php the_time('jS F Y') ?></strong></time> in <?php the_category(', '); ?></p>
+			</header>
 		
-		<p class="entry">
-		  <?php the_content(); ?>
-		</p>
+			<p class="entry">
+			  <?php the_content(); ?>
+			</p>
 
-		<footer>
-			<?php comments_link(); ?> 
-		</footer>
-		
-	</article>	
+			<footer>
+				<?php if ( comments_open() ) : ?><a class="comment" href="<?php the_permalink(); ?>#comments"><?php comments_number('0 Comments', '1 Comment', '% Comments'); ?></a><?php endif; ?>
+				<?php the_tags('<span class="tags">Tagged as ', ', ', '</span>'); ?>
+			</footer>
+		</article>	
+	</div>
 <?php endwhile; else: ?>
 	<p>Sorry, no posts matched your criteria.</p>
 <?php endif; ?>
--- blogs/themes/mysociety/single.php
+++ blogs/themes/mysociety/single.php
@@ -1,23 +1,27 @@
 <?php get_header();?>
  <!-- Start the Loop. -->
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php if ( have_posts() ) : ?>
+	
+	<div class="blog-entries">
+		<?php while ( have_posts() ) : the_post(); ?>
 
-	<article class="blog-post">
+		<article class="blog-post" id="post-<?php the_ID(); ?>">
 
-		<header>
-			<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-			<p class="meta">Written by <?php the_author_posts_link() ?> on <?php the_time('F jS, Y') ?> in <?php the_category(', '); ?></p>
-		</header>
+			<header>
+				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+				<p class="meta">Written by <?php the_author_posts_link() ?> on <?php the_time('F jS, Y') ?> in <?php the_category(', '); ?></p>
+			</header>
 		
-		<p class="entry">
-		  <?php the_content(); ?>
-		</p>
+		    <?php the_content(); ?>
 
-		<footer>
-			<?php get_comments(); ?> 
-		</footer>
-		
-	</article>	
+			<footer>
+				<?php the_tags('<span class="tags">Tagged as ', ', ', '</span>'); ?>
+			</footer>
+			
+			<?php comments_template(); ?>
+		</article>	
+	
+	</div>
 <?php endwhile; else: ?>
 	<p>Sorry, no posts matched your criteria.</p>
 <?php endif; ?>

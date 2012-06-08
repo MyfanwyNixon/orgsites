f06775b55a953f4c62fd9855a496bcb7344ae10c
Jedidiah Broadbent <hello@jedidiah.eu>
2012-06-08 11:47:39 +0100

    mysociety.org: Added isotope back into the frontpage JS

--- blogs/themes/mysociety/js/scripts.js
+++ blogs/themes/mysociety/js/scripts.js
@@ -5,11 +5,46 @@ var tempcolfix=function(){var a=$(".section-options ul").removeClass("four_up").
 
 $(document).ready(function() {
 	
-	// Setup the lightbox for any internal image links
+// Setup the lightbox for any internal image links
 	//setupZoom();
 	
-	// Make options boxes the same height
+// Make options boxes the same height
 	tempcolfix();
 	$(".section-options p.desc").equalHeights();
 	$(".section-options h3").equalHeights();
+	
+
+// Homepage specific scripts
+	if($('body').hasClass('homepage')){
+		
+		var productList = $('.product-list');
+		var filters = $('.product-filters a');
+		
+		// Count down for ordering the products in the same order as loaded
+		var orderCountdown = productList.children('li').length * 4;
+		
+		productList.isotope({
+			itemSelector : 'li',
+			layoutMode : 'fitRows',
+			getSortData : {
+				for_orgs : function( $elem ) {
+					if($elem.hasClass('for_orgs')){ return orderCountdown--; }
+					else{ return 0; }
+				},
+				for_public : function( $elem ) {
+					if($elem.hasClass('for_public')){ return orderCountdown--; }
+					else{ return 0; }
+				},
+				for_volunteers : function( $elem ) {
+					if($elem.hasClass('for_volunteers')){ return orderCountdown--; }
+					else{ return 0; }
+				},
+				for_international : function( $elem ) {
+					if($elem.hasClass('for_international')){ return orderCountdown--; }
+					else{ return 0; }
+				}
+			}
+		});
+		
+	}
 });

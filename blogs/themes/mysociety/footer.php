b794bd32981f89cb0dd145bcfecb7bedd5501d9f
Jedidiah Broadbent <hello@jedidiah.eu>
2012-06-10 00:49:02 +0100

    mysociety.org: Added charity and company number to the footer
    fixes #133

--- blogs/themes/mysociety/footer.php
+++ blogs/themes/mysociety/footer.php
@@ -9,9 +9,11 @@
 		
 		<div id="footer_blurb">
 			<p>We build websites that give the public simple, tangible ways to connect with and improve their society. As well as offering tools directly to the public we provide integration and development services for local authorities, corporates and government. As a UK Charity our own work has a national focus but our vision is global,  we open source our projects and encourage international adaptation.</p>
+			
 		</div>
 		
 	</section>
+	<p class="charity_number"><strong>mySociety</strong> limited (<strong>05798215</strong>) is a project of <strong>UK Citizens Online Democracy</strong> (<abbr title="UK Citizens Online Democracy">UKCOD</abbr>) a registered charity in England and Wales, charity number <strong>1076346</strong>, company number <strong>03277032</strong>.</p>
 </footer>
 
 <script src="<?php bloginfo('template_url'); ?>/js/libs/jquery-1.7.2.min.js" type="text/javascript" charset="utf-8"></script>

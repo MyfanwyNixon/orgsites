c97ca222a73d6301924d1c892082da938020e0c0
Jedidiah Broadbent <hello@jedidiah.eu>
2012-06-11 13:44:03 +0100

    mysociety.org: Removed links to other blogs in sidebar

--- blogs/themes/mysociety/sidebar.php
+++ blogs/themes/mysociety/sidebar.php
@@ -4,10 +4,12 @@
 		<h3>Our Blogs:</h3>
 		<ul>
 			<li><a class="mysociety" href="<?php get_bloginfo( 'url', 'raw' );?>">Company Blog</a></li>
-			<li><a class="fms" href="#">FixMyStreet Blog</a></li>
-			<li><a class="fmt" href="#">FixMyTransport Blog</a></li>
-			<li><a class="twfy" href="#">TheyWorkForYou Blog</a></li>
-			<li><a class="" href="#">Bloggity Blog Blog</a></li>
+			<li><a class="diy" href="http://diy.mysociety.org/">DIY mySociety Blog</a></li>
+<?php
+			//<li><a class="fmt" href="#">FixMyTransport Blog</a></li>
+			//<li><a class="twfy" href="#">TheyWorkForYou Blog</a></li>
+			//<li><a class="" href="#">Bloggity Blog Blog</a></li>
+?>
 		</ul>
 	</section>
 	

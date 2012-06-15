e3585c7414103d26a3537ddca9e2ca4932e2f724
Jedidiah Broadbent <hello@jedidiah.eu>
2012-06-15 11:57:51 +0100

    mysociety.org : Big responsiveness update

--- blogs/themes/mysociety/js/scripts.js
+++ blogs/themes/mysociety/js/scripts.js
@@ -6,19 +6,69 @@ var tempcolfix=function(){var a=$(".section-options ul").removeClass("four_up").
 // IE6 PNG fix - http://allinthehead.com/retro/338/supersleight-jquery-plugin
 jQuery.fn.supersleight=function(a){a=jQuery.extend({imgs:true,backgrounds:true,shim:"x.gif",apply_positioning:true},a);return this.each(function(){if(jQuery.browser.msie&&parseInt(jQuery.browser.version,10)<7&&parseInt(jQuery.browser.version,10)>4){jQuery(this).find("*").andSelf().each(function(d,f){var b=jQuery(f);if(a.backgrounds&&b.css("background-image").match(/\.png/i)!==null){var c=b.css("background-image");var h=c.substring(5,c.length-2);var g=(b.css("background-repeat")==="no-repeat"?"crop":"scale");var e={filter:"progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+h+"', sizingMethod='"+g+"')","background-image":"url("+a.shim+")"};b.css(e);}if(a.imgs&&b.is("img[src$=png]")){var e={width:b.width()+"px",height:b.height()+"px",filter:"progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+b.attr("src")+"', sizingMethod='scale')"};b.css(e).attr("src",a.shim);}if(a.apply_positioning&&b.is("a, input")&&(b.css("position")===""||b.css("position")==="static")){b.css("position","relative");}});}});};
 
+// iPhone ZoomFix https://gist.github.com/901295
+(function(e){var c="addEventListener",b="gesturestart",g="querySelectorAll",f=[1,1],d=g in e?e[g]("meta[name=viewport]"):[];function a(){d.content="width=device-width,minimum-scale="+f[0]+",maximum-scale="+f[1];e.removeEventListener(b,a,true);}if((d=d[d.length-1])&&c in e){a();f=[0.25,1.6];e[c](b,a,true);}}(document));
 
 $(document).ready(function() {
 	
-// Setup the lightbox for any internal image links
-	setupZoom();
-	
-// Make options boxes the same height
 	tempcolfix();
-	$(".section-options p.desc").equalHeights();
-	$(".section-options h3").equalHeights();
-	$(".section-features li").equalHeights();
+	
+// media query event handler
+	if (matchMedia) {
+		var mq = window.matchMedia("(min-width: 60em)");
+		mq.addListener(WidthChange);
+		WidthChange(mq);
+	}
+// media query change
+	function WidthChange(mq) {
+		if (mq.matches) {
+		// window width is at least 60em
+			// Make options boxes the same height
+			$(".section-options p.desc").equalHeights();
+			$(".section-options h3").equalHeights();
+			$(".section-features li").equalHeights();
+			
+			
+			// Homepage specific scripts
+			if($('body').hasClass('homepage')){
+				
+				// Count down for ordering the products in the same order as loaded
+				var orderCountdown = productList.children('li').length * 4;
+				
+				productList.isotope({
+					itemSelector : 'li',
+					layoutMode : 'fitRows',
+					getSortData : {
+						for_orgs : function( $elem ) {
+							if($elem.hasClass('for_orgs')){ return orderCountdown--; }
+							else{ return 0; }
+						},
+						for_public : function( $elem ) {
+							if($elem.hasClass('for_public')){ return orderCountdown--; }
+							else{ return 0; }
+						},
+						for_volunteers : function( $elem ) {
+							if($elem.hasClass('for_volunteers')){ return orderCountdown--; }
+							else{ return 0; }
+						},
+						for_international : function( $elem ) {
+							if($elem.hasClass('for_international')){ return orderCountdown--; }
+							else{ return 0; }
+						}
+					}
+				});
+				
+			}
+		}
+		else {
+			// window width is less than 500px
+			
+			
+		}
+	}
 
-// Homepage specific scripts
+	
+	// Homepage specific scripts
 	if($('body').hasClass('homepage')){
 		
 		var productList = $('.product-list');
@@ -40,32 +90,9 @@ $(document).ready(function() {
 				return false;
 			});
 		});
-		
-		// Count down for ordering the products in the same order as loaded
-		var orderCountdown = productList.children('li').length * 4;
-		
-		productList.isotope({
-			itemSelector : 'li',
-			layoutMode : 'fitRows',
-			getSortData : {
-				for_orgs : function( $elem ) {
-					if($elem.hasClass('for_orgs')){ return orderCountdown--; }
-					else{ return 0; }
-				},
-				for_public : function( $elem ) {
-					if($elem.hasClass('for_public')){ return orderCountdown--; }
-					else{ return 0; }
-				},
-				for_volunteers : function( $elem ) {
-					if($elem.hasClass('for_volunteers')){ return orderCountdown--; }
-					else{ return 0; }
-				},
-				for_international : function( $elem ) {
-					if($elem.hasClass('for_international')){ return orderCountdown--; }
-					else{ return 0; }
-				}
-			}
-		});
-		
 	}
+	
+	
+// Setup the lightbox for any internal image links
+	setupZoom();
 });

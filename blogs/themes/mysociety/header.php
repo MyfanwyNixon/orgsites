2b6712b87978a6ddaf295d40900711e4f634bc63
Jedidiah Broadbent <hello@jedidiah.eu>
2012-06-19 12:27:27 +0100

    mysociety.org: Re: #146 Should hopefully fix the text overflowing when typekit can't load the font

--- blogs/themes/mysociety/header.php
+++ blogs/themes/mysociety/header.php
@@ -22,7 +22,7 @@
 	        elseif ( is_404() ) { bloginfo('name'); print ' | Not Found'; }
 	        else { bloginfo('name'); wp_title('|'); get_page_number(); }
 	?></title>
-	
+	<noscript><link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'></noscript>
 	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" type="text/css" media="screen, print" charset="utf-8">
 	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/print.css" type="text/css" media="print" charset="utf-8">
 	<!--[if IE]><link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/ie.css" type="text/css" media="screen, print" charset="utf-8"><![endif]-->

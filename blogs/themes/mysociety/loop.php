a27c6c4b882047530b7ddf64d57936f60dbff72f
Struan Donald <struan@exo.org.uk>
2012-07-04 15:35:45 +0100

    tidy up linked blog code so we can add in article classes

--- blogs/themes/mysociety/loop.php
+++ blogs/themes/mysociety/loop.php
@@ -1,12 +1,24 @@
 <?php while ( have_posts() ) : the_post(); ?>
-<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+<?php
+    $is_blog_post = 1;
+    $extra_classes = 'blog-post';
 
-  <?php if (!property_exists( $post, 'data') || !$post->data || $post->data == 'NULL') { ?>
+    if (property_exists( $post, 'data') && $post->data && $post->data != 'NULL') {
+        $data = unserialize( base64_decode( $post->data ) );
+
+        $is_blog_post = 0;
+        // we should probably have a mapping for this somewhere but I'm not sure where
+        $extra_classes = 'linked-blog-post linked-blog-' . $data['linked_parent']['blog_id'];
+    } ?>
+
+<article <?php post_class($extra_classes) ?> id="post-<?php the_ID(); ?>">
+
 	<header>
 		<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 		<p class="meta">Written by <span class="author-link"><?php the_author_posts_link() ?></span> on <time datetime="<?php the_time('Y-m-d')?>">Posted <strong><?php the_time('jS F Y') ?></strong></time> in <span class='category-link'><?php the_category(', '); ?></span></p>
 	</header>
 
+  <?php if ($is_blog_post) { ?>
 	<p class="entry">
 	  <?php the_content(); ?>
 	</p>
@@ -15,13 +27,6 @@
 		<?php if ( comments_open() ) : ?><a class="comment" href="<?php the_permalink(); ?>#comments"><?php comments_number('0 Comments', '1 Comment', '% Comments'); ?></a><?php endif; ?>
 		<?php the_tags('<span class="tags">Tagged as ', ', ', '</span>'); ?>
 	</footer>
-  <?php } else {
-      $data = unserialize( base64_decode( $post->data ) );
-  ?>
-	<header>
-    <h2><a class="blog_<?php echo $data['linked_parent']['blog_id'] ?>" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-		<p class="meta">Written by <span class="author-link"><?php the_author_posts_link() ?></span> on <time datetime="<?php the_time('Y-m-d')?>">Posted <strong><?php the_time('jS F Y') ?></strong></time> in <span class='category-link'><?php the_category(', '); ?></span></p>
-	</header>
   <?php } ?>
 </article>	
 <?php endwhile;?>

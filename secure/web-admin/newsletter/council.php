a106367c098ebcfc48206dedbff0e3bed5b6c241
Struan Donald <struan@exo.org.uk>
2012-07-25 15:31:13 +0100

    add council newsletter formatter

---
+++ secure/web-admin/newsletter/council.php
@@ -0,0 +1,9 @@
+<?
+
+require_once(dirname(__FILE__) . '/../../conf/general');
+
+$newsletter = 'council';
+$email_test = OPTION_NEWSLETTER_COUNCIL_TEST_LIST;
+$email_live = OPTION_NEWSLETTER_COUNCIL_LIVE_LIST;;
+
+require_once(dirname(__FILE__) . '/core.php');

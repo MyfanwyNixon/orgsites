26a20795f9744893e91aa80b89e20635c297700a
Struan Donald <struan@exo.org.uk>
2012-08-14 10:58:56 +0100

    put GA snippet in the correct place

--- blogs/themes/mysociety/footer.php
+++ blogs/themes/mysociety/footer.php
@@ -29,5 +29,19 @@
     <script>window.attachEvent("onload",function(){CFInstall.check({mode:"overlay"})})</script>
   <![endif]-->
 
+    <?php if ( !OPTION_STAGING ) { ?>
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-660910-12']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
+    <?php } ?>
+	
 </body>
 </html>
--- blogs/themes/mysociety/header.php
+++ blogs/themes/mysociety/header.php
@@ -32,20 +32,6 @@
 	<script type="text/javascript" src="http://use.typekit.com/bth0qpr.js"></script>
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
-    <?php if ( !OPTION_STAGING ) { ?>
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-660910-12']);
-      _gaq.push(['_trackPageview']);
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-    </script>
-    <?php } ?>
-	
 	<link rel="shortcut icon" href="/favicon.ico">
 
     <? wp_head(); ?>

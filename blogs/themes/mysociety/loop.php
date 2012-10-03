37b7822dc002e3ee5f7c059d3d397b24abd2ec2b
Matthew Somerville <matthew@mysociety.org>
2012-10-03 11:25:36 +0100

    Add older/newer links at bottom for #188 (can't do top with 'paged' as stylesheet doesn't have WP defaults in it).

--- blogs/themes/mysociety/loop.php
+++ blogs/themes/mysociety/loop.php
@@ -30,3 +30,11 @@
   <?php } ?>
 </article>	
 <?php endwhile;?>
+
+<?php if (  $wp_query->max_num_pages > 1 ) : ?>
+    <div id="nav-below" class="navigation">
+        <div class="nav-previous"><?php next_posts_link( '<span class="meta-nav">&larr;</span> Older posts' ); ?></div>
+        <div class="nav-next"><?php previous_posts_link( 'Newer posts <span class="meta-nav">&rarr;</span>' ); ?></div>
+    </div><!-- #nav-below -->
+<?php endif; ?>
+

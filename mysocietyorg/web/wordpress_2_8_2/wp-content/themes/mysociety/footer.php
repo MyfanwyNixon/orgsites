452d8033d5542e4d71a16e065677014e2086cc27
Matthew Somerville <matthew@mysociety.org>
2012-10-18 14:50:42 +0100

    And CEE.

--- mysocietyorg/web/wordpress_2_8_2/wp-content/themes/mysociety/footer.php
+++ mysocietyorg/web/wordpress_2_8_2/wp-content/themes/mysociety/footer.php
@@ -22,25 +22,6 @@ Its company number is 03277032, and mySociety Ltd's is 05798215.
 
 wp_footer();
 
-if ($_SERVER['SERVER_NAME'] == 'cee.mysociety.org') {
-    $piwik_id = 11;
-} else {
-    $piwik_id = 5;
-}
-
 ?>
-<!-- Piwik -->
-<script type="text/javascript">
-var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.mysociety.org/" : "http://piwik.mysociety.org/");
-document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-</script><script type="text/javascript">
-try {
-var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php",  <?=$piwik_id ?>);
-piwikTracker.trackPageView();
-piwikTracker.enableLinkTracking();
-} catch( err ) {}
-</script><noscript><p><img src="http://piwik.mysociety.org/piwik.php?idsite=<?=$piwik_id ?>" style="border:0" alt=""/></p></noscript>
-<!-- End Piwik Tag -->
-
 </body>
 </html>

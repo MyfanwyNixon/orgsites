71f20a27cf91bf607fce350a50aecb8e427cffbc
Louise Crow <louise.crow@gmail.com>
2012-10-24 13:08:24 +0100

    Fix links, update URLs.

--- blogs/themes/alaveteli/custom-home.php
+++ blogs/themes/alaveteli/custom-home.php
@@ -4,8 +4,8 @@ Template Name: Home page
 */
 get_header(); ?>
 
-	<div id="general_frontpage" role="main">
-            
+    <div id="general_frontpage" role="main">
+
   <div id="frontpage_splash">
     <div id="left_column">
       <h1>
@@ -13,14 +13,14 @@ get_header(); ?>
         <span> Make requests in public.
 Hold corporations and governments to account.</span>
        </h1>
-      <a class="link_button_green_large" href="/about-2">Learn more »</a>
+      <a class="link_button_green_large" href="/about">Learn more »</a>
     </div>
     <div id="right_column">
       <div id="frontpage_search_box">
         <h2>
-	  Alaveteli has helped citizens ask more than<br>
+      Alaveteli has helped citizens ask more than<br>
           <strong>100,000</strong> Freedom of Information requests in
-	  <a href="/about-2/where-is-alaveteli-been-installed/"><strong>5</strong> jurisdictions</a>, such as:
+      <a href="/about/where-is-alaveteli-been-installed/"><strong>5</strong> jurisdictions</a>, such as:
         </h2>
 
       </div>
@@ -34,32 +34,32 @@ Hold corporations and governments to account.</span>
   </div>
 
 <div id="frontpage_examples">
-    
+
       <div id="examples_0">
         <h3>What does Alaveteli do?</h3>
         <ul>
-	  <li>Lowers the barriers that prevent citizens asking questions of those in power</li>
-	  <li>Out-of-the-box support for making Freedom of Information requests to public bodies</li>
-	  <li>Sends requests via normal email channels, and publishes responses online (with attachments) instantly</li>
+      <li>Lowers the barriers that prevent citizens asking questions of those in power</li>
+      <li>Out-of-the-box support for making Freedom of Information requests to public bodies</li>
+      <li>Sends requests via normal email channels, and publishes responses online (with attachments) instantly</li>
           <li>Integrates well with social media</li>
-	  <li>Users can subscribe to email alerts matching any words or phrases they like</li>
+      <li>Users can subscribe to email alerts matching any words or phrases they like</li>
           <li>Responses are categorised by users for easy searching and reporting</li>
-	  <li>Translated into 8 languages (and counting...)</li>
+      <li>Translated into 10 languages (and counting...)</li>
         </ul>
 
       </div>
-    
+
 
     <div id="examples_1">
       <h3>How do I get started?</h3>
         <ul>
-          
+
             <li>
                Download <a href="/wp-uploads/2012/02/Turbo-Transparency-v1.0.pdf">Turbo Transparency</a>, a paper about how and why to use Alaveteli for Freedom of Information systems</li>
-            <li>Find out more <a href="/about-2">about the Alaveteli project</a></li>
-            <li>Read the <a href="https://github.com/sebbacon/alaveteli/wiki/Getting-started">Getting Started guide</a> on our wiki</li>
+            <li>Find out more <a href="/about">about the Alaveteli project</a></li>
+            <li>Read the <a href="https://github.com/mysociety/alaveteli/wiki/Getting-started">Getting Started guide</a> on our wiki</li>
             <li>If you are an organisation who wants to use Alaveteli in your jurisdiction, or a developer who is interested in collaborating on the software, please send a message introducing yourself to our <a href="https://groups.google.com/group/alaveteli-dev">developers mailing list</a> or our <a href="https://groups.google.com/group/alaveteli-users">users mailing list</a></li>
-            <li>Developers might want to start with the <a href="https://github.com/sebbacon/alaveteli/wiki/Developers-guide">technical overview</a>.</a>
+            <li>Developers might want to start with the <a href="https://github.com/mysociety/alaveteli/wiki/Developers-guide">technical overview</a>.</a>
         </ul>
     </div>
 </div>

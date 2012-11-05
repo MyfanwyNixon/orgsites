723a5891f0b6ff65bb1f82fae3fc521a0bf3b752
Louise Crow <louise.crow@gmail.com>
2012-11-05 11:31:11 +0000

    Repoint references to test.alaveteli.org to www.alaveteli.org.

--- blogs/themes/alaveteli/footer.php
+++ blogs/themes/alaveteli/footer.php
@@ -5,13 +5,13 @@
     </div>
 
     <div id="footer">
- <img src="http://test.alaveteli.org/images/twitter-16.png" alt="twitter icon" class="twitter-icon"> <a href="http://www.twitter.com/alaveteli_foi">Follow us on twitter</a>
+ <img src="http://www.alaveteli.org/images/twitter-16.png" alt="twitter icon" class="twitter-icon"> <a href="http://www.twitter.com/alaveteli_foi">Follow us on twitter</a>
 
 </div>
 <div class="after-footer">&nbsp;</div>
 
 
-    
+
 
 </div>
 <div id="other-country-notice"></div>
--- blogs/themes/alaveteli/header.php
+++ blogs/themes/alaveteli/header.php
@@ -11,23 +11,23 @@
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
 <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-        <link href="http://test.alaveteli.org/stylesheets/main.css?1328784189" media="all" rel="stylesheet" title="Main" type="text/css">
-        <link href="http://test.alaveteli.org/stylesheets/fonts.css?1314786482" media="all" rel="stylesheet" type="text/css">
-        <link href="http://test.alaveteli.org/stylesheets/print.css?1328784189" media="print" rel="stylesheet" type="text/css">
-        
-        <!--[if LT IE 7]> 
-        <style type="text/css">@import url("/stylesheets/ie6.css");</style> 
+        <link href="http://www.alaveteli.org/stylesheets/main.css?1328784189" media="all" rel="stylesheet" title="Main" type="text/css">
+        <link href="http://www.alaveteli.org/stylesheets/fonts.css?1314786482" media="all" rel="stylesheet" type="text/css">
+        <link href="http://www.alaveteli.org/stylesheets/print.css?1328784189" media="print" rel="stylesheet" type="text/css">
+
+        <!--[if LT IE 7]>
+        <style type="text/css">@import url("/stylesheets/ie6.css");</style>
         <![endif]-->
-        <!--[if LT IE 7]> 
-        <style type="text/css">@import url("/stylesheets/ie6-custom.css");</style> 
+        <!--[if LT IE 7]>
+        <style type="text/css">@import url("/stylesheets/ie6-custom.css");</style>
         <![endif]-->
-        <!--[if LT IE 8]> 
-        <style type="text/css">@import url("/stylesheets/ie7.css");</style> 
+        <!--[if LT IE 8]>
+        <style type="text/css">@import url("/stylesheets/ie7.css");</style>
         <![endif]-->
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
-<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?> 
+<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 
 <?php wp_head(); ?>
 <script type="text/javascript">
@@ -49,29 +49,29 @@
 <div class="entirebody">
     <div id="banner">
       <div id="banner_inner">
-        
-        
+
+
           <div id="logged_in_bar">
- <img src="http://test.alaveteli.org/images/twitter-16.png" alt="twitter icon" class="twitter-icon"> <a href="http://www.twitter.com/alaveteli_foi">Follow us on twitter</a>
+ <img src="http://www.alaveteli.org/images/twitter-16.png" alt="twitter icon" class="twitter-icon"> <a href="http://www.twitter.com/alaveteli_foi">Follow us on twitter</a>
 
           </div>
-           
+
 
         <div id="navigation_search">
             <form action="/" id="navigation_search_form" method="get">
                <p>
                    <input id="navigation_search_query" name="s" size="40" type="text" value="<?php the_search_query(); ?>">
-                   <input src="http://test.alaveteli.org/images/search-button.png?1314786482" type="image">
+                   <input src="http://www.alaveteli.org/images/search-button.png?1314786482" type="image">
                </p>
             </form>
         </div>
-        
-        <a href="/" id="logo"><img src="http://test.alaveteli.org/alavetelitheme/images/logo.png?1328785848" alt="Logo"></a>
 
-        
+        <a href="/" id="logo"><img src="http://www.alaveteli.org/alavetelitheme/images/logo.png?1328785848" alt="Logo"></a>
+
+
         <div id="topnav">
           <ul>
-			<?php wp_list_pages('title_li=&depth=1' ); ?>
+      <?php wp_list_pages('title_li=&depth=1' ); ?>
           </ul>
 
     </ul>
@@ -81,8 +81,8 @@
     </div>
     <div id="wrapper">
         <div id="content">
-            
-            
+
+
 
             <div id="help_about" class="controller_help">
-            
+

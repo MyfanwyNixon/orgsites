e73202b0d46a4833b7153c5784716d1980351c0a
Matthew Somerville <matthew@mysociety.org>
2012-12-21 14:37:20 +0000

    Fix nonsecure asset paths.

--- secure/web/donate/index.php
+++ secure/web/donate/index.php
@@ -2,8 +2,7 @@
 
 $file = file_get_contents('http://www.mysociety.org/donate/');
 $file = preg_replace('#href="/#', 'href="http://www.mysociety.org/', $file);
-$file = preg_replace('#(rel="stylesheet" href=")http://www.mysociety.org/#', '$1/nonsecure/', $file);
-$file = preg_replace('#src="/#', 'src="/nonsecure/', $file);
-$file = preg_replace('#<!-- Piwik.*?/Piwik -->#s', '', $file);
+$file = preg_replace('#(rel="stylesheet" href=")http://www.mysociety.org/wp-content/themes/mysociety/#', '$1/nonsecure/', $file);
+$file = preg_replace('#src="/wp-content/themes/mysociety/#', 'src="/nonsecure/', $file);
 print $file;
 

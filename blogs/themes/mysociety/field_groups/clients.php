53f85143a88c8daf869e304d18f4106afb6db87b
Ben Nickolls <ben@mysociety.org>
2013-01-11 15:31:25 +1300

    Updating client and funder custom field definitions
    exported PHP from v.2.2.5 Advanced custom fields

--- blogs/themes/mysociety/field_groups/clients.php
+++ blogs/themes/mysociety/field_groups/clients.php
@@ -4,115 +4,83 @@
  * You may edit the array as you see fit. However, this may result in errors if the array is not compatible with ACF
  * This code must run every time the functions.php file is read
  */
-
 if(function_exists("register_field_group"))
 {
-	register_field_group(array (
-		'id' => '50e4e9f605f83',
-		'title' => 'Clients',
-		'fields' => 
-		array (
-			0 => 
-			array (
-				'key' => 'field_11',
-				'label' => 'Clients',
-				'name' => 'clients',
-				'type' => 'repeater',
-				'order_no' => 0,
-				'instructions' => 'A list of clients',
-				'required' => 0,
-				'conditional_logic' => 
-				array (
-					'status' => 0,
-					'rules' => 
-					array (
-						0 => 
-						array (
-							'field' => 'null',
-							'operator' => '==',
-						),
-					),
-					'allorany' => 'all',
-				),
-				'sub_fields' => 
-				array (
-					'field_12' => 
-					array (
-						'label' => 'Name',
-						'name' => 'name',
-						'type' => 'text',
-						'instructions' => 'The name of the client',
-						'column_width' => '',
-						'default_value' => '',
-						'formatting' => 'html',
-						'order_no' => 0,
-						'key' => 'field_12',
-					),
-					'field_13' => 
-					array (
-						'label' => 'Image',
-						'name' => 'image',
-						'type' => 'image',
-						'instructions' => 'An image or logo to accompany the client ',
-						'column_width' => '',
-						'save_format' => 'url',
-						'preview_size' => 'thumbnail',
-						'order_no' => 1,
-						'key' => 'field_13',
-					),
-					'field_14' => 
-					array (
-						'label' => 'Description',
-						'name' => 'description',
-						'type' => 'textarea',
-						'instructions' => 'A description of our involvement with the client (formatted as HTML)',
-						'column_width' => '',
-						'default_value' => '',
-						'formatting' => 'html',
-						'order_no' => 2,
-						'key' => 'field_14',
-					),
-				),
-				'row_min' => 0,
-				'row_limit' => '',
-				'layout' => 'row',
-				'button_label' => 'Add Client',
-			),
-		),
-		'location' => 
-		array (
-			'rules' => 
-			array (
-				0 => 
-				array (
-					'param' => 'page_template',
-					'operator' => '==',
-					'value' => 'clients.php',
-					'order_no' => '0',
-				),
-			),
-			'allorany' => 'all',
-		),
-		'options' => 
-		array (
-			'position' => 'normal',
-			'layout' => 'no_box',
-			'hide_on_screen' => 
-			array (
-				0 => 'excerpt',
-				1 => 'discussion',
-				2 => 'comments',
-				3 => 'revisions',
-				4 => 'slug',
-				5 => 'author',
-				6 => 'format',
-				7 => 'featured_image',
-				8 => 'categories',
-				9 => 'tags',
-				10 => 'send-trackbacks',
-			),
-		),
-		'menu_order' => 0,
-	));
-}
-?>
+register_field_group(array (
+  'id' => '50ef7072a958d',
+  'title' => 'Clients',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'key' => 'field_50ef6f7b972aa',
+      'label' => 'Clients',
+      'name' => 'clients',
+      'type' => 'repeater',
+      'instructions' => 'Add a new client',
+      'required' => '1',
+      'sub_fields' => 
+      array (
+        0 => 
+        array (
+          'key' => 'field_50ef6f7b97321',
+          'label' => 'name',
+          'name' => 'name',
+          'type' => 'text',
+          'default_value' => '',
+          'formatting' => 'none',
+          'order_no' => '0',
+        ),
+        1 => 
+        array (
+          'key' => 'field_50ef6f7b97388',
+          'label' => 'description',
+          'name' => 'description',
+          'type' => 'textarea',
+          'default_value' => '',
+          'formatting' => 'html',
+          'order_no' => '1',
+        ),
+        2 => 
+        array (
+          'key' => 'field_50ef6f7b973f2',
+          'label' => 'image',
+          'name' => 'image',
+          'type' => 'image',
+          'save_format' => 'url',
+          'preview_size' => 'thumbnail',
+          'order_no' => '2',
+        ),
+      ),
+      'row_limit' => '',
+      'layout' => 'row',
+      'button_label' => 'Add Client',
+      'order_no' => '0',
+    ),
+  ),
+  'location' => 
+  array (
+    'rules' => 
+    array (
+      0 => 
+      array (
+        'param' => 'page_template',
+        'operator' => '==',
+        'value' => 'clients.php',
+        'order_no' => '0',
+      ),
+    ),
+    'allorany' => 'all',
+  ),
+  'options' => 
+  array (
+    'position' => 'normal',
+    'layout' => 'default',
+    'hide_on_screen' => 
+    array (
+		0 => 'excerpt'
+    ),
+  ),
+  'menu_order' => 0,
+));
+}?>
--- blogs/themes/mysociety/field_groups/funders.php
+++ blogs/themes/mysociety/field_groups/funders.php
@@ -1,5 +1,4 @@
-<?php
-/**
+<?/**
  * Register field groups
  * The register_field_group function accepts 1 array which holds the relevant data to register a field group
  * You may edit the array as you see fit. However, this may result in errors if the array is not compatible with ACF
@@ -8,55 +7,54 @@
 if(function_exists("register_field_group"))
 {
 register_field_group(array (
-  'id' => '4fc35498a9865',
+  'id' => '50ef708e19a16',
   'title' => 'Funders',
   'fields' => 
   array (
     0 => 
     array (
-      'key' => 'field_4fb4edd1496d8',
       'label' => 'Funders',
       'name' => 'funders',
       'type' => 'repeater',
-      'instructions' => 'Add a partner\'s details',
+      'instructions' => 'A list of funders',
       'required' => '0',
       'sub_fields' => 
       array (
         0 => 
         array (
-          'key' => 'field_4fb4edd1496fb',
           'label' => 'Name',
           'name' => 'name',
           'type' => 'text',
           'default_value' => '',
           'formatting' => 'none',
+          'key' => 'field_50ef703c38e68',
           'order_no' => '0',
         ),
         1 => 
         array (
-          'key' => 'field_4fb4edd14970a',
           'label' => 'Icon',
           'name' => 'icon',
           'type' => 'image',
           'save_format' => 'url',
           'preview_size' => 'thumbnail',
+          'key' => 'field_50ef703c38ecf',
           'order_no' => '1',
         ),
         2 => 
         array (
-          'key' => 'field_4fb4edd149716',
-          'label' => 'Detail',
+          'label' => 'detail',
           'name' => 'detail',
           'type' => 'textarea',
-		  'formatting' => 'html',
-          'toolbar' => 'basic',
-          'media_upload' => 'no',
+          'default_value' => '',
+          'formatting' => 'html',
+          'key' => 'field_50ef703c38f3a',
           'order_no' => '2',
         ),
       ),
       'row_limit' => '',
       'layout' => 'row',
-      'button_label' => '+ Add Funder',
+      'button_label' => 'Add Funder',
+      'key' => 'field_50ef703c38e01',
       'order_no' => '0',
     ),
   ),
@@ -78,14 +76,16 @@ register_field_group(array (
   array (
     'position' => 'normal',
     'layout' => 'default',
-    'show_on_page' => 
+    'hide_on_screen' => 
     array (
-		0 => 'the_content',
-	  	1 => 'slug',
-		2 => 'custom_fields',
+      0 => 'excerpt',
+      1 => 'discussion',
+      2 => 'comments',
+      3 => 'author',
+      4 => 'format',
+      5 => 'featured_image',
     ),
   ),
-  'menu_order' => 2,
+  'menu_order' => 0,
 ));
-}
-?>
+}?>

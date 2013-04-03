f4b6c1a7576d77afe2c2f864c73d882d37e18028
Matthew Somerville <matthew@mysociety.org>
2013-04-03 15:06:13 +0100

    Noddy script to dump db table.

---
+++ secure/web-admin/donate/index.php
@@ -0,0 +1,15 @@
+<?php
+# Noddy script to dump monthly donors
+
+$dir = dirname(__FILE__);
+require_once "$dir/../conf/general";
+require_once "$dir/../../commonlib/phplib/db.php";
+
+header('Content-Type: text/csv; charset=utf-8');
+
+db_connect();
+$out = pg_copy_to($db_h, 'donor', ',');
+foreach ($out as $row) {
+    print $row;
+}
+

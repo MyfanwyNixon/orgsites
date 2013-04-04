02a9758899de4c408ccd9d09d7494c99ae3e4445
Mike Thompson <mysociety.mike@gmail.com>
2013-04-04 19:10:05 +0200

    Update maps_ad.php to remove broken links and link to Mapumental

--- mysocietyorg/includer/maps_ad.php
+++ mysocietyorg/includer/maps_ad.php
@@ -1,14 +1,10 @@
 <div id="divProductHeadline" class="contenthalf aligncenter">
-	<!-- Header -->
-	<h1>Commuting Time Maps</h1>
+	<h2>Travel Time Maps</h2>
 	<p>
-		<a href="/projects/travelmaps/">A World First - Custom maps that help house hunters and office managers find the best places quick and for easy commutes</a>
+	  	The work described in this page has led to the creation of <a href="http://www.mapumental.com">Mapumental</a>, a service that provides custom maps that help house hunters and office managers find the best places for quick and easy commutes.
 	</p>
-
-	<!-- More info -->
-	<a class="linkbutton" href="/services/request-a-quote/?p=map">
-        <span class="left">&nbsp;</span>
-        <span class="middle">Get a quote for your map now</span>                
-        <span class="right">&nbsp;</span>
-    </a>
+	<p>
+		<a class="btn" href="http://shop.mapumental.com/">Buy a custom map on Mapumental</a>
+		<a class="btn" href="http://property.mapumental.com/">Find houses on Mapumental Property</a>
+	</p>
 </div>
